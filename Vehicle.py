f3e1b8356d2d3be97a96de041982bb505f4484fa
Cory Grewohl <corywalter.grewohl@calbaptist.edu>
2021-10-23 16:33:07 -0700

    first part of python work

---
+++ Vehicle.py
@@ -0,0 +1,23 @@
+class Vehicle:
+    def __init__(self, make, model, model_year, is_4_wheel_drive, retail_price, mpg):
+        self.make = make
+        self.model = model
+        self.modelYear = model_year
+        self.is4WheelDrive = is_4_wheel_drive
+        self.retailPrice = retail_price
+        self.MPG = mpg
+
+
+class Car(Vehicle):
+    def __init__(self, make, model, model_year, is_4_wheel_drive, retail_price, mpg, is_convertible):
+        self.is_convertible = is_convertible
+
+        Vehicle.__init__(self, make, model, model_year, is_4_wheel_drive, retail_price, mpg)
+
+
+class Truck(Vehicle):
+    def __init__(self, make, model, model_year, is_4_wheel_drive, retail_price, mpg, has_side_step, tow_capacity):
+        self.has_side_step = has_side_step
+        self.tow_capacity = tow_capacity
+
+        Vehicle.__init__(self, make, model, model_year, is_4_wheel_drive, retail_price, mpg)
---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from Vehicle import Vehicle, Car, Truck
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+v = Vehicle("Ford", "F150", 2010, False, 25000, 12)
+print(v.make)
+print(v.model)
